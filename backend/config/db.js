e811aa703440e9ac559de85c85cbcb93377dc996
pavan modi <pavanmodi@pavans-MacBook-Air.local>
2026-05-31 20:35:46 +0530

    Update database connection configuration

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,27 +1,12 @@
 import mongoose from "mongoose";
 
-// const connectDB = async () => {
-//   const uri = process.env.MONGO_URI;
-//   if (!uri) {
-//     console.error("MONGO_URI not set in .env");
-//     process.exit(1);
-//   }
-//   try {
-//     await mongoose.connect(uri, { dbName: "college_complaints" });
-//     console.log("MongoDB connected");
-//   } catch (err) {
-//     console.error("MongoDB connection error:", err.message);
-//     process.exit(1);
-//   }
-// };
-
-// export default connectDB;
 const connectDB = async () => {
-  try{
-    await mongoose.connect(process.env.MONGO_URI)
-    console.log("MongoDB connected");
-  }catch{
-    console.error("MongoDB connection error");
+  try {
+    mongoose.connection.on('connected', () => console.log("Database Connected"));
+    await mongoose.connect(`${process.env.MONGODB_URI}/nextgen-complaint-system`)
+  } catch (error) {
+    console.log(error.message);
   }
 }
+
 export default connectDB;
